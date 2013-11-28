07db2bae8ae5d5aaca4203c8e188d05bef806e03
Sean Kelley <seansfkelley@gmail.com>
2013-11-28 13:38:36 -0500

    Initial Commit

---
+++ drinks/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  drinks
+//
+//  Created by Sean Kelley on 11/28/13.
+//  Copyright (c) 2013 Sean Kelley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

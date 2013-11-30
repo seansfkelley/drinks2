e99d27dfeece03b0058c5d112d469eba3efc3e13
Sean Kelley <seansfkelley@gmail.com>
2013-11-30 14:22:19 -0500

    Moving stuff around in Xcode.

--- RecipesViewController.m
+++ RecipesViewController.m
@@ -71,12 +71,6 @@
     }
 
     self.index = [[RecipeIndex alloc] initWithRecipes:self.recipesList withIngredients:self.ingredientsList withFudgeFactor:FUDGE_FACTOR];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning

2080e8835253928f360e924eced50f96005a1e88
Sean Kelley <seansfkelley@gmail.com>
2013-12-23 03:27:20 -0500

    Simple add/remove table.

--- CustomDrinkTableViewController.m
+++ CustomDrinkTableViewController.m
@@ -8,32 +8,60 @@
 
 #import "CustomDrinkTableViewController.h"
 
+typedef enum rowTypeEnum {
+    CUSTOM_CONTENT,
+    SINGLE_EDITABLE,
+    ADDABLE,
+    EMPTY
+} RowType;
+
+@interface RowItem : NSObject
+
+@property RowType type;
+@property NSString *text;
+
+- (id)initWithType:(RowType)type withText:(NSString *)text;
+
+@end
+
+@implementation RowItem
+
+- (id)initWithType:(RowType)type withText:(NSString *)text {
+    self = [super init];
+    if (self) {
+        self.type = type;
+        self.text = text;
+    }
+    return self;
+}
+
+@end
+
 @interface CustomDrinkTableViewController ()
+
+@property NSMutableArray *rows;
 
 @end
 
 @implementation CustomDrinkTableViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
+
+    self.editing = YES;
+
+    self.rows = [[NSMutableArray alloc] initWithArray:@[
+        [[RowItem alloc] initWithType:SINGLE_EDITABLE withText:@"Name"],
+        [[RowItem alloc] initWithType:EMPTY withText:nil],
+        [[RowItem alloc] initWithType:ADDABLE withText:@"add Ingredient"]
+    ]];
+    // This is goofy: this class gets tapped as a data source before viewDidLoad, so we need to
+    // force a reload. Putting this in initWithStyle doesn't seem to help.
+    [self.tableView reloadData];
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
-
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
-
 
 #pragma mark - Table view data source
 
@@ -42,15 +70,29 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+    return [self.rows count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"CustomDrinkPrototypeCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    RowItem *r = [self rowItemForIndexPath:indexPath];
 
-    // Configure the cell...
+    cell.textLabel.font = [UIFont systemFontOfSize:14.0f];
+    cell.textLabel.text = r.text; // How to make nice blue color?
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+//    cell.userInteractionEnabled = NO;
+
+    switch (r.type) {
+        case ADDABLE:
+//            cell.userInteractionEnabled = YES; // But no style -- don't want to flash colors.
+            break;
+        case CUSTOM_CONTENT:
+        case SINGLE_EDITABLE:
+        case EMPTY:
+            break;
+    }
 
     return cell;
 }
@@ -60,13 +102,44 @@
 //}
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    RowItem *r = [self rowItemForIndexPath:indexPath];
+    switch (r.type) {
+        case CUSTOM_CONTENT:
+            NSAssert(editingStyle == UITableViewCellEditingStyleDelete, @"CUSTOM_CONTENT must have delete editing style.");
+            [self.rows removeObjectAtIndex:indexPath.row];
+            [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        case SINGLE_EDITABLE:
+        case EMPTY:
+            break;
+        case ADDABLE: {
+            NSAssert(editingStyle == UITableViewCellEditingStyleInsert, @"ADDABLE must have insert editing style.");
+            RowItem *newRow = [[RowItem alloc] initWithType:CUSTOM_CONTENT withText:@"test"];
+            [self.rows insertObject:newRow atIndex:indexPath.row];
+            [tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationRight];
+            break;
+        }
     }
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+}
+
+- (RowItem *)rowItemForIndexPath:(NSIndexPath *)indexPath {
+    return [self.rows objectAtIndex:indexPath.row];
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    switch ([self rowItemForIndexPath:indexPath].type) {
+        case CUSTOM_CONTENT:
+            return UITableViewCellEditingStyleDelete;
+        case SINGLE_EDITABLE:
+        case EMPTY:
+            return UITableViewCellEditingStyleNone;
+        case ADDABLE:
+            return UITableViewCellEditingStyleInsert;
     }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+
 }
 
 /*
